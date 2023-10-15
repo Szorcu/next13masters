abf85c3b14078fcbac0dcb241acd01e20a94ab0f
Norbert Szorc <norbertszorc@bluerider.software>
2023-10-15 18:09:14 +0200

    Navbar adjustments

--- src/ui/organisms/Navbar.tsx
+++ src/ui/organisms/Navbar.tsx
@@ -4,8 +4,8 @@ import { SearchBox } from "@ui/atoms/SearchBox"
 
 export const Navbar = () => {
 	return (
-		<nav className="border-b-1 flex h-16 items-center justify-between gap-4 border-b border-slate-200 bg-white px-6">
-			<ul className="flex h-full items-center gap-8">
+		<nav className="border-b-1 sticky top-0 flex h-16 items-center justify-between gap-4 border-b border-slate-200 bg-white px-6">
+			<ul className="flex h-full items-center gap-8 text-gray-500">
 				<NavbarLink href="/" text="Home" />
 				<NavbarLink href="/products" text="All" exact={false} />
 				<NavbarLink href="/categories/t-shirts" text="T-shirts" exact={false} />
