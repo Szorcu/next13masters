90284f3a6abf6f8a9a5372995539d9bfcbbf0553
Norbert Szorc <norbertszorc@bluerider.software>
2023-10-16 18:08:39 +0200

    fixed navbar struture to pass tests

--- src/ui/organisms/Navbar.tsx
+++ src/ui/organisms/Navbar.tsx
@@ -6,21 +6,22 @@ import { SearchBox } from "@ui/atoms/SearchBox"
 
 export const Navbar = () => {
 	return (
-		<nav className="border-b-1 sticky top-0 z-10 flex h-16 items-center justify-between gap-4 border-b border-slate-200 bg-white px-6">
-			<ul className="flex h-full items-center gap-8 text-gray-500">
-				<NavbarLink href="/" text="Home" />
-				<NavbarLink href="/products" text="All" exact={false} />
-				<NavbarLink href="/categories/t-shirts" text="T-shirts" exact={false} />
-				<NavbarLink href="/categories/hoodies" text="Hoodies" exact={false} />
-				<NavbarLink href="/categories/accessories" text="Accessories" exact={false} />
-			</ul>
-
+		<div className="border-b-1 sticky top-0 z-10 flex h-16 items-center justify-between gap-4 border-b border-slate-200 bg-white px-6">
+			<nav className="h-full">
+				<ul className="flex h-full items-center gap-8 text-gray-500">
+					<NavbarLink href="/" text="Home" />
+					<NavbarLink href="/products" text="All" exact={false} />
+					<NavbarLink href="/categories/t-shirts" text="T-shirts" exact={false} />
+					<NavbarLink href="/categories/hoodies" text="Hoodies" exact={false} />
+					<NavbarLink href="/categories/accessories" text="Accessories" exact={false} />
+				</ul>
+			</nav>
 			<div className="flex w-full max-w-20rem items-center gap-4">
 				<SearchBox />
 				<Link href="/cart">
 					<ShoppingCart className="text-sm text-gray-500 hover:cursor-pointer hover:text-yellow-400" />
 				</Link>
 			</div>
-		</nav>
+		</div>
 	)
 }
