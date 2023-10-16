083692a3070a6dd8aea43527898e0e8642cf836d
Norbert Szorc <norbertszorc@bluerider.software>
2023-10-16 17:53:01 +0200

    Modified home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,23 @@
-import { notFound } from "next/navigation"
 import { getCollections } from "@api/calls/getCollections"
 import { CollectionList } from "@ui/organisms/CollectionList"
+import { getProducts } from "@api/calls/getProducts"
+import { ProductList } from "@ui/organisms/ProductList"
 
 const Home = async () => {
 	const collections = await getCollections()
-
-	if (collections.length === 0) {
-		notFound()
-	}
+	const products = await getProducts(1)
 
 	return (
-		<section>
-			<CollectionList {...{ collections }} />
-		</section>
+		<main className="flex flex-col gap-32">
+			<section>
+				<CollectionList {...{ collections }} />
+			</section>
+
+			<section className="flex flex-col gap-6">
+				<h1 className="text-xl font-bold text-gray-700">Featured products</h1>
+				<ProductList {...{ products }} />
+			</section>
+		</main>
 	)
 }
 
